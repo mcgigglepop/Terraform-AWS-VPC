83f80faaa67797c75bc25e498d1252a7ea2676c1
cristopher ontiveros <christoph.alano@gmail.com>
2020-12-13 02:11:00 -0600

    added ec2 specific variables

--- variables.tf
+++ variables.tf
@@ -37,10 +37,46 @@ variable "description" {
 
   default = "Sample VPC Architecture"
 }
-
 variable "tags" {
   description = "Tags to apply to resources"
   type        = map(string)
 
   default = {}
+}
+
+# EC2 Specific Variables
+variable "ec2_ami" {
+  description = "Map of Ubuntu Minimal AMIs by region"
+  type        = map(string)
+
+  default = {
+    us-east-1    = "ami-7029320f"
+    us-east-2    = ""
+    us-west-1    = ""
+    us-west-2    = ""
+    eu-central-1 = ""
+    eu-west-1    = ""
+  }
+}
+variable "ec2_instance_type" {
+  description = "EC2 instance type"
+  type        = string
+
+  default = "t2.micro"
+}
+variable "ec2_key_name" {
+  description = "EC2 SSH Key"
+  type        = string
+}
+variable "ec2_root_volume_size" {
+  description = "Size of the root volume (in Gigabytes)"
+  type        = string
+
+  default = 8
+}
+variable "ec2_root_volume_type" {
+  description = "Type of the root volume (standard, gp2, or io)"
+  type        = string
+
+  default = "gp2"
 }
